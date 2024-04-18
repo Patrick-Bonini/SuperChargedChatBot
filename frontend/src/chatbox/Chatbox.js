9b8bbd739a0d66bd101e0dcad8c779b38c181c52
Fabrice Mikobi <fabricemikobi15@gmail.com>
2024-04-18 15:26:10 -0400

    changed styling

--- frontend/src/chatbox/Chatbox.js
+++ frontend/src/chatbox/Chatbox.js
@@ -2,52 +2,45 @@ import React, { useState, useRef } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 import StraightIcon from '@mui/icons-material/Straight';
 
+import { CircularProgress } from "@mui/material";
 const Chatbox = ({ isOpen }) => {
-  const [messages, setMessages] = useState([]);
+
+  const initMessage = {text: "Hello! I'm Solly, your virtual assistant. How can I help you today?", sender: "bot"}
+  const [messages, setMessages] = useState([initMessage]);
   const [input, setInput] = useState("");
   const scrollRef = useRef();
 
-  const sendMessage = async (event) => {
+
+  const sendMessage = (event) => {
     event.preventDefault();
     if (input) {
       setMessages([...messages, { text: input, sender: "user" }]);
       setInput("");
-      try {
-        const response = await fetch("http://127.0.0.1:5000", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ input }),
-        });
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        // Assume your backend responds with a message property
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          { text: data.message, sender: "bot" },
-        ]);
-      } catch (error) {
-        console.error("Error sending message:", error);
-      }
+
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { text: 'Solly is thinking...', sender: "bot" },
+      ]);
+
     }
   };
-  
 
   return (
     isOpen && (
       <div className={`chatbox ${isOpen ? "open" : ""}`}>
+
         <ScrollToBottom
           className="message-container"
           scrollViewClassName="messages"
           ref={scrollRef}
         >
           {messages.map((message, index) => (
-            <div key={index} className={`message ${message.sender}`}>
+            <div>
+              <div key={index} className={`message ${message.sender}`}>
               {message.text}
+              </div>
             </div>
+            
           ))}
         </ScrollToBottom>
         <form className="message-form" onSubmit={sendMessage}>
@@ -59,6 +52,7 @@ const Chatbox = ({ isOpen }) => {
           />
           <button type="submit" style={{backgroundColor: "#00C895", borderRadius: "10px", padding: "0px 8px 0px 8px"}}><StraightIcon sx={{ fontSize: '1.3rem' }}/></button>
         </form>
+
       </div>
     )
   );
