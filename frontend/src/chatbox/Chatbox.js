e4c4dc57e8a67338d2edc2f84258746d85a38190
Fabrice Mikobi <fabricemikobi15@gmail.com>
2024-04-18 15:51:22 -0400

    added linear progress

--- frontend/src/chatbox/Chatbox.js
+++ frontend/src/chatbox/Chatbox.js
@@ -1,12 +1,12 @@
 import React, { useState, useRef } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 import StraightIcon from '@mui/icons-material/Straight';
-
+import { LinearProgress, Stack } from "@mui/material";
 const Chatbox = ({ isOpen }) => {
 
   const initMessage = {text: "Hello! I'm Solly, your virtual assistant. How can I help you today?", sender: "bot"}
   const [messages, setMessages] = useState([initMessage]);
-
+  const [isLoading, setIsLoading] = useState(false);
   const [input, setInput] = useState("");
   const scrollRef = useRef();
 
@@ -15,6 +15,7 @@ const Chatbox = ({ isOpen }) => {
     if (input) {
       setMessages([...messages, { text: input, sender: "user" }]);
       setInput("");
+      setIsLoading(true);
       try {
         const response = await fetch("http://127.0.0.1:5000", {
           method: "POST",
@@ -28,6 +29,7 @@ const Chatbox = ({ isOpen }) => {
         }
         const data = await response.json();
         // Assume your backend responds with a message property
+        setIsLoading(false);
         setMessages((prevMessages) => [
           ...prevMessages,
           { text: data.message, sender: "bot" },
@@ -53,6 +55,12 @@ const Chatbox = ({ isOpen }) => {
             </div>
           ))}
         </ScrollToBottom>
+        
+        {isLoading &&  <Stack sx={{backgroundColor: "#C7F5EA", color: "#00C898"}}>
+         <LinearProgress color={"inherit"}></LinearProgress>
+        </Stack>}
+         
+
         <form className="message-form" onSubmit={sendMessage}>
           <input
             type="text"
