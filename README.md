c4e7d5dac98d4927827478b55a3ddd4e4409ada2
Neel Patel <>
2024-04-24 08:56:07 -0400

    CLI Branch initial commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Setup Instructions
-1. Clone repository
-`git clone https://github.com/Patrick-Bonini/SuperChargedChatBot`
+1. Clone this branch
+`git clone -b cli https://github.com/Patrick-Bonini/SuperChargedChatBot`
 
 2. Create an env file `.env` in the root directory with the contents:
 
@@ -12,8 +12,31 @@ Fill in the key and id with valid keys and ids
 
 3. Create a virtual environment and install the requirements
 
-`pip install -r requirements.txt`
+```
+pip install -r requirements.txt
+```
+or 
+```
+pip3 install -r requirements.txt
+```
 
 4. Run with 
 
-`python chat.py`
+```
+python chat.py
+```
+or 
+```
+python3 chat.py
+```
+
+## Usage
+
+Upon running `chat.py`, you will be asked to enter a URL. This can be any URL, but if the web scraper fails to get a response from the page, then it will default to SDKPerf's documentation page. You can then ask questions relating to the contents of the webpage that you entered.
+
+To exit from the chat and enter another URL, type in any of the following commands in the chat:
+```
+'q', 'quit', 'leave', 'url', 'exit', 'esc', 'escape'
+```
+
+This same set of commands also allows you to exit the program as well from the `Enter URL:` prompt.
--- web_scrape.py
+++ web_scrape.py
@@ -13,7 +13,7 @@ def scrapeWebsite(url):
             title = ' '.join([p.get_text() for p in soup.find_all(['h1'])])
             print(f'topic = {title}')
 
-            formatted_content = f"I want you to act as Solly, an expert on {title}. Do not include an initial line in the response, only the answer to the request. The user will provide you with details about needing assistance setting up or using {title}, and your role is to suggest the most suitable solution to the user's problem. You should use your knowledge of {title}, coding languages, Linux, etc., in order to develop a comprehensive solution to the problem. If the user does not seem to have a problem that involves {title} or anything related, you should treat it as a normal request and act as an AI information tool or refer them to a different page in the docs that would better match their request.\n\nHere is your knowledge on {title}: {text_content}"
+            formatted_content = f"As Solly, an expert on {title}, your task is to provide brief and precise assistance to users seeking help with {title}. Respond directly to user queries without introductory lines. Use your expertise in {title}, coding, Linux, etc., to offer concise solutions in 150 characters or less. If the inquiry doesn't relate to {title}, respond as a general AI assistant or guide users to relevant documentation. Your {title} knowledge: {text_content}"
 
             formatted_content = autopep8.fix_code(formatted_content, options={'aggressive': 1})
 
@@ -24,6 +24,6 @@ def scrapeWebsite(url):
             print(f'Failed to scrape URL. Status code: {response.status_code}')
 
     except Exception as e:
-        print(f'{e}\n Defaulting to PubSub+ Platform Documentation')
-        # Default to PubSub+ Platform if webscrape is unsuccessful
-        scrapeWebsite('https://docs.solace.com/Get-Started/Solace-PubSub-Platform.htm')
+        print(f'{e}\n Defaulting to SDKPerf Documentation')
+        # Default to SDKPerf Documentation if webscrape is unsuccessful
+        scrapeWebsite('https://docs.solace.com/API/SDKPerf/SDKPerf.htm')
